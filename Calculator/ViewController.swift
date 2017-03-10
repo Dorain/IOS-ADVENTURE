54763317d51cdd30ee51fe4adeb60066eb8d4b62
Dorain <patrickgu001@gmail.com>
2017-03-09 23:26:21 -0800

    calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func touchDigit(_ sender: UIButton) {
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    func drawHorizonLine(from startX:Double, to endX:Double, using color:UIColor)
 }
 
