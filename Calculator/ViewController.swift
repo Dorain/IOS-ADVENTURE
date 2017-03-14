29ec1d238f1d9e3d6ff245458531e01496429202
Dorain <patrickgu001@gmail.com>
2017-03-13 21:32:41 -0700

    updated

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    var digitInMiddle = false
+    
     @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if digitInMiddle {
+            let textCurrent = display!.text!
+            display!.text = textCurrent + digit
+        }
+        else{
+            display!.text = digit
+            digitInMiddle = true
+        }
     }
-    func drawHorizonLine(from startX:Double, to endX:Double, using color:UIColor)
+//    func drawHorizonLine(from startX:Double, to endX:Double, using color:UIColor)
 }
 
