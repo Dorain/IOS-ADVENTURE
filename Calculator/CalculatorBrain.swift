ea5df188e306ecc9e7da80326ad5d6187b5154c0
Dorain <patrickgu001@gmail.com>
2017-03-31 15:51:15 -0700

    updated

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,29 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by 顾禹 on 3/31/17.
+//  Copyright © 2017 G Inc. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+    private var accumulator: Double?
+    
+    func performOperation(_ symbol:String) {
+        
+    }
+    
+    mutating func setOperand(_ operand:Double){
+        
+    }
+    
+    var result: Double{
+        get {
+            return accumulator
+        }
+    }
+    
+}
